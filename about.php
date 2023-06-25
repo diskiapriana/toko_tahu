1d83a66e452f4bb937201db38143ea2426529c1e
Diski Apriana <diskiapriana@gmail.com>
2023-06-25 22:02:34 +0700

    first commit

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #ff8680"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Pabrik Tahu Kencana adalah salah satu pabrik penghasil tahu putih di Indonesia. Produk kami sehat, bergizi, dan terjangkau oleh semua orang.</p>
+
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
